036bc634ed94ff21c09a917d2d849c9204ad2f93
benezrd-dev <benezrd@post.bgu.ac.il>
2026-01-13 11:06:02 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
+import socket
+import struct
+import sys
 
+# Protocol Constants
+MAGIC_COOKIE = 0xabcddcba
+MSG_OFFER = 0x02
+MSG_REQUEST = 0x03
+MSG_PAYLOAD = 0x04
+
+UDP_PORT = 13122
+TEAM_NAME = "Team 2"
+
+def unpack_offer(data):
+    # !IBH32s: Cookie, Type, Port, Name
+    return struct.unpack('!IBH32s', data)
